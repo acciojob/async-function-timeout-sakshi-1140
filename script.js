50255affb724b5e57572998b7a28d5c947d207c6
Sakshi Choudhary <103510144+sakshi-1140@users.noreply.github.com>
2024-06-29 00:32:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-//your JS code here. If required.
+// Event listener for button click
+const button =document.getElementByID('btn')
+button.addEventListener('click',submitfunction);
+
+async function submitfunction(){
+  const inputValue=document.getElementByID('text').value;
+  const delayValue=document.getElementByID('delay').value;
+  const output=document.getElementByID('output');
+   //output.textContent=5;
+  await delay(Number(delayValue))
+  
+  //changed value
+  output.textContent=inputValue;
+}
+
+const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
+// delay function in detail
+
+// function delay(ms){
+//   const myPromise= new Promise(function(resolve){
+//     setTimeout(resolve,ms);
+//   })
+// }
